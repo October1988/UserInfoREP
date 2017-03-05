73a9f46ce836b74eac7b95ecdb5d6b37db26732d
Admin <admin@Mac-Admin.local>
2017-03-05 17:41:24 +0200

    With Bridging Header And Cocoa Touch classes for controller and cell

---
+++ UserInfoApp/UserInfoTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoTVCell.swift
+//  UserInfoApp
+//
+//  Created by Admin on 05.03.17.
+//  Copyright © 2017 Freeman. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
